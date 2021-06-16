e61bef71beece49f6b25b6891ead7a87f5f87c14
Javier <jmnoeda2000@gmail.com>
2021-06-16 14:03:06 +0200

    Ejercicio Terminado

--- app/src/main/java/com/cbellmont/ejemploadaptersinlista/StringAdapter.kt
+++ app/src/main/java/com/cbellmont/ejemploadaptersinlista/StringAdapter.kt
@@ -12,7 +12,7 @@ class StringAdapter : RecyclerView.Adapter<StringAdapter.StringViewHolder>()  {
 
     class StringViewHolder(var root: View, var textView: TextView) : RecyclerView.ViewHolder(root)
 
-    var numeroDeFilas = 5
+    var numeroDeFilas = 10
 
     // Aquí solo obtenemos las vistas que están dentro de layout en el que queremos mostrar nuestros elementos
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StringViewHolder {
@@ -23,13 +23,26 @@ class StringAdapter : RecyclerView.Adapter<StringAdapter.StringViewHolder>()  {
 
     // Aquí devolvemos el número de elementos que habrá en nuestro recylcerView
     override fun getItemCount(): Int {
-        return numeroDeFilas
+        return numeroDeFilas +  1
     }
+
 
     // Aquí ponermos en el layout vacío que hemos creado en el onCreateViewHolder los datos del elemento
     override fun onBindViewHolder(holder: StringViewHolder, position: Int) {
         holder.textView.text = position.toString()
-    }
 
+        if (position < numeroDeFilas){
+            holder.textView.text=if(position%2!=0){
+                "Soy Impar"
+            } else {
+                holder.textView.setBackgroundColor(ContextCompat.getColor(holder.root.context, R.color.colorPrimary))
+                "Posicion = $position"
+            }
+        } else {
+            if (position == numeroDeFilas){
+                holder.textView.text="Soy el último"
+            }
+        }
+    }
 }
 
